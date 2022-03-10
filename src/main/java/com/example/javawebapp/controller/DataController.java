5fb98acc293d701db2195ab5744b295e968d5e70
Louis Wan <chwanlouis@gmail.com>
2022-03-10 10:10:34 +0800

    update config file

--- src/main/java/com/example/javawebapp/controller/DataController.java
+++ src/main/java/com/example/javawebapp/controller/DataController.java
@@ -1,7 +1,5 @@
 package com.example.javawebapp.controller;
 
-import com.example.javawebapp.model.Bar;
-import com.example.javawebapp.pojo.ReturnsPojo;
 import com.example.javawebapp.pojo.ReturnsResponsePojo;
 import com.example.javawebapp.pojo.SymbolPojo;
 import com.example.javawebapp.service.AlphavantageDataService;
@@ -11,12 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.List;
 
 @RestController
+@RequestMapping(value={"/api"})
 public class DataController {
 
     Logger logger = LoggerFactory.getLogger(DataController.class);
@@ -24,20 +20,20 @@ public class DataController {
     @Autowired
     AlphavantageDataService alphavantageDataService;
 
-    @RequestMapping(value={"/api/getReturns"})
+    @RequestMapping(value={"/getReturns"})
     public ReturnsResponsePojo getReturns(@RequestParam String symbol, @RequestParam String from,
                                           @RequestParam String to) {
         logger.info(String.format("Requesting: /api/getReturns?symbol=%s&from=%s&to=%s", symbol, from, to));
         return alphavantageDataService.requestAnnualizedReturns(symbol, from, to);
     }
 
-    @RequestMapping(value={"/api/getSymbol"})
+    @RequestMapping(value={"/getSymbol"})
     public List<SymbolPojo> symbolSearch(@RequestParam String keyword) {
         logger.info(String.format("Requesting: /api/getSymbol?keyword=%s", keyword));
         return alphavantageDataService.requestSymbolSearch(keyword);
     }
 
-    @RequestMapping(value={"/api/test"})
+    @RequestMapping(value={"/test"})
     public String testing() {
         logger.info("Requesting: /api/test");
         return "testing";
